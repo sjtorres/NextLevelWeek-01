64972a09d2ac680da4c1e353419812fdaf07f923
Sandro Torres <sandrotorres.ti@gmail.com>
2020-07-28 15:05:07 -0300

    Data users

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,51 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de Usuários')
+app.use(express.json())
 
-  response.json([
-    "Sandro",
-    "Patrícia",
-    "Gabriela",
-    "Isabela",
-    "Rafael"
-  ])
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
+// GET: Buscar uma ou mais informação do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+const users = [
+  "Sandro",   //0
+  "Patrícia", //1
+  "Gabriela", //2
+  "Isabela",  //3
+  "Rafael"    //4
+]
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search)
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+
+  return response.json(filteredUsers)
+})
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const user = users[id]
+
+  return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+  const data = request.body
+
+  console.log(data)
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  }
+
+  return response.json(user)
 })
 
 app.listen(3333)

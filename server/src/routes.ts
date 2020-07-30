523767d47da8aaf77fe20c46a4d3de426031e0ca
Sandro Torres <sandrotorres.ti@gmail.com>
2020-07-30 06:49:43 -0300

    Controller Points add

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import knex from './database/connection'
+import PointsController from './controllers/PointsController'
 
 const routes = express.Router()
+const pointsController = new PointsController
 
 routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*')
@@ -17,39 +19,6 @@ routes.get('/items', async (request, response) => {
   return response.json(serializedItems)
 })
 
-routes.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body
-
-  const ids = await knex('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  })
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id: ids[0]
-    }
-  })
-
-  await knex('point_items').insert(pointItems)
-
-  return response.json({ success: true })
-})
+routes.post('/points', pointsController.create)
 
 export default routes

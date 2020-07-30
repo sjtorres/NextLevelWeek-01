83b7c108345877d03c3ac09823d7c2f957afc040
Sandro Torres <sandrotorres.ti@gmail.com>
2020-07-30 05:34:57 -0300

    Seeds add

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -3,8 +3,8 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('points', table => {
     table.increments('id').primary();
-    table.string('image').notNullable();
     table.string('name').notNullable();
+    table.string('image').notNullable();
     table.string('email').notNullable();
     table.string('whatsapp').notNullable();
     table.decimal('latitude').notNullable();
